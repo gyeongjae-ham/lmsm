cc56b0c620c17e990a24e35cab603e90e08be0e8
Gyeongjae Ham <gyeongjae.h.dev@gmail.com>
2025-07-12 15:03:58 +0900

    Revert "[Feature/problem calculator and enums] (#2)"
    This reverts commit 5e6f7e9b512329b2963cd60597dccc0d0173933e.

--- core/core-enum/src/main/kotlin/com/lms/enum/exception/InvalidLevelValueException.kt
+++
@@ -1,8 +0,0 @@
-package com.lms.enum.exception
-
-import org.apache.coyote.BadRequestException
-
-class InvalidLevelValueException(
-    levelValue: Int,
-    message: String = "Invalid level value $levelValue"
-) : BadRequestException(message)
