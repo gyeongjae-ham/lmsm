8c53a3acf8bf4b5d2042c94db4c3fa3dce2924b0
Gyeongjae Ham <luca@wemeetmobility.com>
2025-07-12 17:36:11 +0900

    feat: ProblemRepository test 추가

--- storage/db-core/build.gradle.kts
+++ storage/db-core/build.gradle.kts
@@ -8,6 +8,6 @@ dependencies {
     implementation(project(":core:core-domain"))
     implementation(project(":core:core-common"))
     implementation("org.flywaydb:flyway-core")
-    runtimeOnly("com.h2database:h2")
+    testImplementation("com.h2database:h2")
     api("org.springframework.boot:spring-boot-starter-data-jpa")
 }
