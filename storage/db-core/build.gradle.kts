484cc222031cc820d47398500e48934dbc9958fc
Gyeongjae Ham <luca@wemeetmobility.com>
2025-07-13 10:41:12 +0900

    chore: h2 database 의존성 추가

--- storage/db-core/build.gradle.kts
+++ storage/db-core/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation(project(":core:core-domain"))
     implementation(project(":core:core-common"))
     implementation("org.flywaydb:flyway-core")
+    implementation("com.h2database:h2")
     testImplementation("com.h2database:h2")
     api("org.springframework.boot:spring-boot-starter-data-jpa")
 }
