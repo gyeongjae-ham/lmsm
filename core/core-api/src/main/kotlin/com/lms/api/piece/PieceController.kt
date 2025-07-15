280ee518090a01746b6e85ca4c6357d5e9ef1906
gyeongjae-ham <gyeongjae.h.dev@gmail.com>
2025-07-15 15:21:31 +0900

    feat: 학생 답변 채점 API 작성

--- core/core-api/src/main/kotlin/com/lms/api/piece/PieceController.kt
+++ core/core-api/src/main/kotlin/com/lms/api/piece/PieceController.kt
@@ -4,10 +4,12 @@ import com.lms.core_common.response.ApiResponse
 import com.lms.core_domain.piece.domain.Piece
 import com.lms.core_domain.piece.domain.request.PieceAssignRequest
 import com.lms.core_domain.piece.domain.request.PieceCreateRequest
+import com.lms.core_domain.piece.domain.request.PieceScoreRequest
 import com.lms.core_domain.piece.domain.request.ProblemOrderUpdateRequest
 import com.lms.core_domain.piece.domain.response.PieceAssignResponse
 import com.lms.core_domain.piece.domain.response.PieceCreateResponse
 import com.lms.core_domain.piece.domain.response.PieceProblemsResponse
+import com.lms.core_domain.piece.domain.response.PieceScoreResponse
 import com.lms.core_domain.piece.domain.response.ProblemOrderResponse
 import com.lms.core_domain.piece.service.PieceService
 import com.lms.core_domain.user.domain.User
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -65,6 +68,18 @@ class PieceController(
         val response = pieceService.getProblemsForStudent(
             pieceId = Piece.PieceId(pieceId),
             studentId = User.UserId(studentId)
+        )
+        return ApiResponse.success(data = response)
+    }
+
+    @PutMapping("/{pieceId}/score")
+    fun scoreAnswers(
+        @PathVariable pieceId: Long,
+        @RequestBody @Valid request: PieceScoreRequest
+    ): ApiResponse<PieceScoreResponse> {
+        val response = pieceService.scoreAnswers(
+            pieceId = Piece.PieceId(pieceId),
+            request = request
         )
         return ApiResponse.success(data = response)
     }
