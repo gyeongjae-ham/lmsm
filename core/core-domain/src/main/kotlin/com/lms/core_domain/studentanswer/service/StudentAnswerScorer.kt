6f53388f0692e6740ed8d9756b388140d8790fbf
gyeongjae-ham <gyeongjae.h.dev@gmail.com>
2025-07-15 15:16:19 +0900

    feat: 학생의 답변 채점 병렬 처리하는 도메인 추가 및 test

---
+++ core/core-domain/src/main/kotlin/com/lms/core_domain/studentanswer/service/StudentAnswerScorer.kt
@@ -0,0 +1,51 @@
+package com.lms.core_domain.studentanswer.service
+
+import com.lms.core_common.exception.BusinessException
+import com.lms.core_domain.piece.domain.Piece
+import com.lms.core_domain.piece.domain.request.PieceScoreRequest
+import com.lms.core_domain.piece.domain.response.ScoreResultResponse
+import com.lms.core_domain.studentanswer.domain.StudentAnswer
+import com.lms.core_domain.user.domain.User
+import org.springframework.stereotype.Service
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
+
+@Service
+class StudentAnswerScorer {
+    private val executor = Executors.newVirtualThreadPerTaskExecutor()
+
+    fun scoreAnswers(
+        studentId: User.UserId,
+        pieceId: Piece.PieceId,
+        piece: Piece,
+        request: PieceScoreRequest
+    ): List<Pair<ScoreResultResponse, StudentAnswer>> {
+        val futures = request.answers.map { answerRequest ->
+            CompletableFuture.supplyAsync({
+                val problem = piece.getProblemsWithSequence()
+                    .find { it.problem.id.value == answerRequest.problemId }
+                    ?.problem
+                    ?: throw BusinessException("Problem not found in piece: ${answerRequest.problemId}")
+
+                val studentAnswer = StudentAnswer.score(
+                    studentId = studentId,
+                    pieceId = pieceId,
+                    problemId = problem.id,
+                    studentAnswer = answerRequest.studentAnswer,
+                    correctAnswer = problem.answer
+                )
+
+                val scoreResult = ScoreResultResponse(
+                    problemId = problem.id.value,
+                    studentAnswer = answerRequest.studentAnswer,
+                    correctAnswer = problem.answer,
+                    isCorrect = studentAnswer.isCorrect()
+                )
+
+                scoreResult to studentAnswer
+            }, executor)
+        }
+
+        return futures.map { it.get() }
+    }
+}
