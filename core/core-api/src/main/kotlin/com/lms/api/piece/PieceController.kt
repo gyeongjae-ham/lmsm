525e5abbcdcbf4494220ac0edcccc44e07ac1139
gyeongjae-ham <gyeongjae.h.dev@gmail.com>
2025-07-15 09:38:55 +0900

    feat: 학습지 문제 조회 API 작성

--- core/core-api/src/main/kotlin/com/lms/api/piece/PieceController.kt
+++ core/core-api/src/main/kotlin/com/lms/api/piece/PieceController.kt
@@ -7,15 +7,19 @@ import com.lms.core_domain.piece.domain.request.PieceCreateRequest
 import com.lms.core_domain.piece.domain.request.ProblemOrderUpdateRequest
 import com.lms.core_domain.piece.domain.response.PieceAssignResponse
 import com.lms.core_domain.piece.domain.response.PieceCreateResponse
+import com.lms.core_domain.piece.domain.response.PieceProblemsResponse
 import com.lms.core_domain.piece.domain.response.ProblemOrderResponse
 import com.lms.core_domain.piece.service.PieceService
+import com.lms.core_domain.user.domain.User
 import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -49,6 +53,18 @@ class PieceController(
         val response = pieceService.assignToStudents(
             pieceId = Piece.PieceId(pieceId),
             request = request
+        )
+        return ApiResponse.success(data = response)
+    }
+
+    @GetMapping("/{pieceId}/problems")
+    fun getProblems(
+        @PathVariable pieceId: Long,
+        @RequestParam studentId: Long
+    ): ApiResponse<PieceProblemsResponse> {
+        val response = pieceService.getProblemsForStudent(
+            pieceId = Piece.PieceId(pieceId),
+            studentId = User.UserId(studentId)
         )
         return ApiResponse.success(data = response)
     }
